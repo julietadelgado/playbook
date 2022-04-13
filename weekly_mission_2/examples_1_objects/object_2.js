687607305cb07b353ea2f0368d74d82d65bd8a3b
Julieta Delgado <julidmeza@gmail.com>
2022-04-12 19:54:21 -0500

    Add objects examples

---
+++ weekly_mission_2/examples_1_objects/object_2.js
@@ -0,0 +1,9 @@
+console.log("Objeto")
+
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+    name: "Julieta",
+    age: 28
+}
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObjetc2)
---
+++ weekly_mission_2/examples_1_objects/object_3.js
@@ -0,0 +1,21 @@
+console.log("Objeto")
+
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Sipiti",
+    age: 2,
+    nicknames: [
+        "Sip",
+        "Sipi"
+    ],
+    address: {
+        zip_code: "10000",
+        street: "Dr. Vertiz 11 Benito Juarez",
+        city: "CDMX"
+    }
+}
+console.log("Ejemplo 3: Objeto con diferentes propiedades")
+console.log(myObject3)
+console.log(`\nName: ${myObject3.name}\n`)
+console.log("Address")
+console.log(myObject3["address"])
